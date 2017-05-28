c6d53debd53337fcecedeea960b49b72ad688c07
sp_angle <kirufin3752@gmail.com>
2017-05-28 23:15:26 +0900

    一つまでの引数の関数作成

---
+++ app/src/main/java/com/sp_angle/app/minecraft_emd/dataBase/Const.java
@@ -0,0 +1,31 @@
+package com.sp_angle.app.minecraft_emd.dataBase;
+
+import com.sp_angle.app.minecraft_emd.R;
+
+import static com.sp_angle.app.minecraft_emd.dataBase.DataBase.editActivityContext;
+
+/**
+ * Created by watanabe-takumi on 2017/05/28.
+ */
+
+public class Const {
+    public static final String SKIP = "スキップ";
+    public static final String PRINT = "画面にメッセージを表示する";
+    public static final String CLIENT_MESSAGE = "チャットに文字を表示する";
+
+    //public static final String
+
+    public static final String LIST_MODE_STRING = "listMode";
+    public static final String VALUE_MODE_STRING = "valueMode";
+
+    public static final String KOUBUN_STRING = "koubun";
+    public static final String FUNCTION_STRING = "function";
+
+
+    public static final String TEXT_STRING = "text";
+
+    public static final String HINT_DISPLAY_TEXT = editActivityContext.getString(R.string.hint_display_text);
+
+    public static final String COMPLETION = editActivityContext.getString(R.string.completion);
+
+}
